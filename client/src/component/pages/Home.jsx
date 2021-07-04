47c4c71cd5fd72e1a71d5ead08de0e39bece791a
Rahi Khan <rahikhan360@gmail.com>
2021-07-04 06:50:59 +0600

    added poppins font

--- client/src/component/pages/Home.jsx
+++ client/src/component/pages/Home.jsx
@@ -1,15 +1,16 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthState";
+import PostForm from "../posts/PostForm";
 
 const Home = () => {
-    const { loadUser } = useContext(AuthContext);
+    // const { loadUser } = useContext(AuthContext);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => loadUser(), []);
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+    // useEffect(() => loadUser(), []);
 
     return (
         <div>
-            <h1>Home MF</h1>
+            <PostForm />
         </div>
     );
 };
